04696c2e87fd793ab46f127dc85698b562e6f4be
David Davydov <kctrlv@gmail.com>
2017-01-09 10:49:17 -0700

    fix included link spec

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -2,10 +2,14 @@ require "rails_helper"
 
 RSpec.describe "can create links", :js => :true do
   scenario "Create a new link" do
-    visit "/"
-    fill_in "Title:", :with => "Turing"
-    fill_in "URL:", :with => "http://turing.io"
-    click_on "Add Link"
+    User.create!(email: "bob@bob.com", password: "1234")
+    visit "/login"
+    fill_in "email", with: "bob@bob.com"
+    fill_in "password", with: "1234"
+    click_on "Submit"
+    fill_in "link-title", :with => "Turing"
+    fill_in "link-url", :with => "http://turing.io"
+    click_button "Add Link"
 
     within('#links-list') do
       expect(page).to have_text("Turing")
