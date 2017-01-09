ad02f275851f37bc58c2d77016efb27fe6547ddf
David Davydov <kctrlv@gmail.com>
2017-01-09 11:12:37 -0700

    user can add links

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,4 +6,18 @@ class Link < ApplicationRecord
       .group("links.url")
       .order('count("reads".id) DESC').limit(10)
   }
+
+  belongs_to :user
+  validates :title, presence: true
+  validates :url, presence: true
+  validate :url_is_valid
+
+  def url_is_valid
+    if url
+      uri = URI.parse(url)
+      unless uri.kind_of?(URI::HTTP) or uri.kind_of?(URI::HTTPS)
+        errors.add(:url, "is not a valid URL")
+      end
+    end
+  end
 end
