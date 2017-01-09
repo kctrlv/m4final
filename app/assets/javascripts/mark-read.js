170e4908be8842c918e4acbece401e9e6f80f43c
David Davydov <kctrlv@gmail.com>
2017-01-09 13:44:19 -0700

    user can mark as read and unread

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -3,10 +3,18 @@ var $newLinkTitle, $newLinkUrl;
 $(document).ready(function(){
   function updateLinkToRead(event) {
     $('#link-' + this).addClass('read')
+    var button = $('#link-' + this).find('.mark-read')
+    button.removeClass('mark-read')
+    button.addClass('mark-unread')
+    button.html("Mark Unread")
   }
 
   function updateLinkToUnread(event) {
     $('#link-' + this).removeClass('read')
+    var button = $('#link-' + this).find('.mark-unread')
+    button.removeClass('mark-unread')
+    button.addClass('mark-read')
+    button.html("Mark as Read")
   }
 
   $('#links-list').on('click', 'button.mark-read', function(){
@@ -28,6 +36,6 @@ $(document).ready(function(){
       url: '/api/v1/links/' + linkId,
       method: 'PATCH',
       data: {read: false}
-    }).then(updateLinkToUnread);
+    }).then(updateLinkToUnread.bind(linkId));
   })
 })
